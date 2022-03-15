b06451ec89e54590a2a88e3979f15e6990fa0a59
ericcarvalho <ericcarvalho935@gmail.com>
2022-03-15 18:18:58 -0300

    adição da estrutura das questões e de uma versão incial do layout

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 ## Sistema de simulado do ENADE ADS 2021.
 
-### Foi desenvolvido um projeto chamado SimuNade, com 5 contribuientes.
+Foi desenvolvido um projeto chamado SimuNade, com 5 contribuientes sob a orientação da Rita de Cassia Felix e coorientação do Jose Luis Barboza Lobianco, ambos docentes na Fatec Carapicuiba.
 
 ## Padrão de nomenclatura adotado:
 
-### Em relação as perguntas temos:
-### Q1E_EQ8 -> onde "Q" significa questão, "1" do número da questão e, "EQ8" significa Equipe 8. O mesmo padrão foi adotado nas imagens, porém sem a alternativa correta inforada.
+Em relação as perguntas temos:
 
+`Q1AE_EQ8` -> onde "Q" significa questão, "1" do número da questão, "A" de alternativa, "E" de alternativa correta e, "EQ8" significa Equipe 8.
+
+`Q2AC_ENADE_ADS_2021_EQ8.png` -> onde "Q" significa questão, "2" do número da questão, "A" de alternativa, "C" de alternativa correta, "ADS" Análise e Desenvolvimento de Sistemas e, "EQ8" significa Equipe 8.
 
---
+++ js/video.js
@@ -0,0 +1,48 @@
+// Make Cards
+const sectionCards = document.querySelector("section.cards");
+
+const card = document.querySelector("div.card");
+
+const videos = [
+  {
+    title: "ENADE // O que é? Como é? Pra que serve? | Mayk Brito",
+    duration: "9:22",
+    thumb: "https://i.ytimg.com/vi/avJZ2mtFljc/hqdefault.jpg",
+    video_id: "avJZ2mtFljc"
+  }
+];
+
+videos.map(video => {
+  const cardClone = card.cloneNode(true);
+  cardClone.setAttribute("id", video.video_id);
+  cardClone.querySelector("img").src = video.thumb;
+  cardClone.querySelector(".title").innerHTML = video.title;
+  cardClone.querySelector(".info > p.text--medium").innerHTML =
+    video.duration;
+  sectionCards.appendChild(cardClone);
+});
+
+card.remove();
+
+// Modal actions
+const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal");
+const cards = [...document.querySelectorAll(".cards .card")];
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    modal.querySelector(
+      "iframe"
+    ).src = `https://www.youtube.com/embed/${card.getAttribute("id")}`;
+    modalOverlay.classList.add("active");
+    modal.classList.add("active");
+    document.querySelector("body").style.overflow = "hidden";
+  });
+});
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+  modal.classList.remove("active");
+  modal.querySelector("iframe").src = ``;
+  document.querySelector("body").style.overflow = "initial";
+});
