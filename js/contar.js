d28dd2e6be1e4b00a664b81233cce4f6ce661a80
ericcarvalho <ericcarvalho935@gmail.com>
2022-04-23 12:41:22 -0300

    codificação da contabilização do gabarito

--- js/contar.js
+++ js/contar.js
@@ -1,3 +1,28 @@
-var altCorreta
+var altCorreta = ''
 var idPagina
 var perguntasRespondidas
+
+/* Armazenando a alternativa correta. */
+altCorreta = document.getElementById('alt-correta').value
+/* Armazenando o ID da página (Questão). */
+idPagina = document.querySelector('Body').id
+
+/* 
+    Verificando a existência de uma sessão
+    para salvar as respostas do usuário. 
+    Se existir...
+*/
+if(sessionStorage.getItem('gabaritoUsuario')){
+    /* Armazenando o gabarito do usuário em uma variável. */
+    perguntasRespondidas = sessionStorage.getItem('gabaritoUsuario')
+    
+
+}
+/* 
+    Senão, criamos um sessionStorage 
+    para armazenamos os dados.
+*/
+else {
+    sessionStorage.setItem('gabaritoUsuario', '')
+    perguntasRespondidas = ''
+}
